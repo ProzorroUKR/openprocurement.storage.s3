24926b7e260abfa33ec7e6b9d9cdb475fc59ea35
Aleksey Stryukov <aleksey.stryukov@gmail.com>
2022-04-05 11:30:20 +0300

    Adding region connection

--- openprocurement/storage/s3/__init__.py
+++ openprocurement/storage/s3/__init__.py
@@ -1,13 +1,20 @@
-from boto.s3.connection import S3Connection
+from boto.s3 import connect_to_region
 from openprocurement.storage.s3.storage import S3Storage
 
 
 def includeme(config):
     settings = config.registry.settings
-    if 's3.access_key' in settings and 's3.secret_key' in settings and 's3.bucket' in settings:
-        # S3 connection
-        connection = S3Connection(settings['s3.access_key'], settings['s3.secret_key'])
-        bucket_name = settings['s3.bucket']
-        config.registry.storage = S3Storage(connection, bucket_name)
+    if (
+        's3.access_key' in settings
+        and 's3.access_key' in settings
+        and 's3.secret_key' in settings
+        and 's3.bucket' in settings
+    ):
+        connection = connect_to_region(
+            settings['s3.region'],
+            aws_access_key_id=settings['s3.access_key'],
+            aws_secret_access_key=settings['s3.secret_key']
+        )
+        config.registry.storage = S3Storage(connection, settings['s3.bucket'])
     else:
         raise ValueError("s3.access_key, s3.secret_key, s3.bucket are required ")
